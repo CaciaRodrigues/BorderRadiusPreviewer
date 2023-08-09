1a5fdd3e9558f721cc444878a810f3ebc3ff62ab
CaciaRodrigues <caciarodriguues@gmail.com>
2023-08-09 18:31:51 -0300

    Refinando o código da manipulação e adicionando a ação de clipboard

--- script.js
+++ script.js
@@ -1,18 +1,54 @@
-const brInput = document.getElementById('border-radiusForm');
-const mutableBox = document.getElementById('mutable-box');
-const textCopy = document.getElementById('border-radius_copy');
+const box = document.getElementById('mutable-box'); // Caixa de demonstração
+const copyBtn = document.getElementById('copyBtn'); // Botão para copiar para o clipboard
+const brCopy = document.getElementById('border-radius_copy'); // Texto para ser copiado usando clipboard
+// Criando um array dos elementos de input para poder manipula-los usando map e forEach
+const cornerInputs = [ 
+    {element: document.getElementById('TopLeft'), name: 'TopLeft'},
+    {element: document.getElementById('TopRight'), name: 'TopRight'},
+    {element: document.getElementById('BottomRight'), name: 'BottomRight'},
+    {element: document.getElementById('BottomLeft'), name: 'BottomLeft'},
+];
 
-brInput.addEventListener('submit', (e) => {
-    e.preventDefault();
+//Função para atualizar o propriedade border-radius da caixa e exibir os valores dentro dela
+function updateBorderRadius() {
+    const brValues = cornerInputs.map(input => `${input.element.value}%`).join(' ');
+    
+    box.style.borderRadius = brValues;
 
-    topLeft = e.target.elements['input-TopLeft'];
-    topRight = e.target.elements['input-TopRight'];
-    bottomLeft = e.target.elements['input-BottomLeft'];
-    bottomRight = e.target.elements['input-BottomRight'];
+    brCopy.style.display = 'block';
+    brCopy.innerHTML = brValues;
 
-    mutableBox.style.borderRadius = `${topLeft.value}px ${topRight.value}px ${bottomRight.value}px ${bottomLeft.value}px`;
+    copyBtn.style.display = 'block';
+}
 
-    textCopy.innerHTML = `${topLeft.value}px ${topRight.value}px ${bottomRight.value}px ${bottomLeft.value}px`
+cornerInputs.forEach(inputData => {
+    inputData.element.addEventListener('input', updateBorderRadius);
+});
 
-    console.log(topLeft.value, topRight.value, bottomLeft.value, bottomRight.value);
-})
+copyBtn.addEventListener('click', function() {
+    const text = brCopy.textContent;
+
+    navigator.clipboard.writeText(text)
+    .then(() => {
+        alert(`Text copied to clipboard: ${text}`)
+    });
+});
+
+// const brInput = document.getElementById('border-radiusForm');
+// const mutableBox = document.getElementById('mutable-box');
+// const textCopy = document.getElementById('border-radius_copy');
+
+// brInput.addEventListener('submit', (e) => {
+//     e.preventDefault();
+
+//     topLeft = e.target.elements['input-TopLeft'];
+//     topRight = e.target.elements['input-TopRight'];
+//     bottomLeft = e.target.elements['input-BottomLeft'];
+//     bottomRight = e.target.elements['input-BottomRight'];
+
+//     mutableBox.style.borderRadius = `${topLeft.value}px ${topRight.value}px ${bottomRight.value}px ${bottomLeft.value}px`;
+
+//     textCopy.innerHTML = `${topLeft.value}px ${topRight.value}px ${bottomRight.value}px ${bottomLeft.value}px`
+
+//     console.log(topLeft.value, topRight.value, bottomLeft.value, bottomRight.value);
+// })
